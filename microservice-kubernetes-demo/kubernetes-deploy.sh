59337075ead341a555a7bfc81f995d7493580f09
vinaysingh121 <43336793+vinaysingh121@users.noreply.github.com>
2019-02-25 14:49:28 +0530

    Update kubernetes-deploy.sh

--- microservice-kubernetes-demo/kubernetes-deploy.sh
+++ microservice-kubernetes-demo/kubernetes-deploy.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 if [ -z "$DOCKER_ACCOUNT" ]; then
-    DOCKER_ACCOUNT=ewolff
+    DOCKER_ACCOUNT=vincracks
 fi;
-kubectl run apache --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-apache:latest --port=80
+kubectl run apache --image=docker.io/$DOCKER_ACCOUNT/apache:latest --port=80
 kubectl expose deployment/apache --type="LoadBalancer" --port 80
-kubectl run catalog --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-catalog:latest --port=8080
+kubectl run catalog --image=docker.io/$DOCKER_ACCOUNT/catalog:latest --port=8080
 kubectl expose deployment/catalog --type="LoadBalancer" --port 8080
-kubectl run customer --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-customer:latest --port=8080
+kubectl run customer --image=docker.io/$DOCKER_ACCOUNT/customer:latest --port=8080
 kubectl expose deployment/customer --type="LoadBalancer" --port 8080
-kubectl run order --image=docker.io/$DOCKER_ACCOUNT/microservice-kubernetes-demo-order:latest --port=8080
+kubectl run order --image=docker.io/$DOCKER_ACCOUNT/order:latest --port=8080
 kubectl expose deployment/order --type="LoadBalancer" --port 8080
